bed0ccec88dfea776578ab9af7eeff0d80ea317e
SJanasek <stella.janasek@gmail.com>
2020-09-07 12:46:44 +0200

    with dt being 0.1

--- seir-model.r
+++ seir-model.r
@@ -8,15 +8,16 @@ beta <- 1/100000 #transmissionsrate
 sigma <- 1/14 #infected to recovered
 gamma <- 1/7 #exposed to infected
 
-out1 <- matrix (0, ncol =4, nrow =200)
-for (i in 1:200){
+#function without dt
+out1 <- matrix (0, ncol =8, nrow =100)
+for (i in 1:100){
   S0n <- S0
   E0n <- E0
   I0n <- I0
   R0n <- R0
-  S0 <-  max(0, S0n -beta*((S0n*I0n)))
-  E0 <- max(0, E0n + beta*((S0n*I0n)) - sigma * E0n)
-  I0 <- max(0, I0n + sigma*E0n - gamma*I0n)
+  S0 <-  max(0, S0n - beta*((S0n*I0n)))
+  E0 <- max(0, E0n + beta*((S0n*I0n)) -  sigma * E0n)
+  I0 <- max(0, I0n +  sigma*E0n -  gamma*I0n)
   R0 <- max(0, R0n + gamma*I0n)
   out1[i,1] <- S0
   out1[i,2] <- E0
@@ -25,10 +26,31 @@ for (i in 1:200){
   
 }
 
-plot (1:200, out1[,1], type ="l", ylim =c(0,82000000))
-lines (1:200, out1[,2], col ="orange" )
-lines (1:200, out1[,3], col ="red" )
-lines (1:200, out1[,4], col ="seagreen")
+#function with dt
+for (i in 1:100){
+  S0n <- S0
+  E0n <- E0
+  I0n <- I0
+  R0n <- R0
+  S0 <-  max(0, S0n - 0.1* beta*((S0n*I0n)))
+  E0 <- max(0, E0n + 0.1* beta*((S0n*I0n)) -  0.1* sigma * E0n)
+  I0 <- max(0, I0n + 0.1* sigma*E0n - 0.1* gamma*I0n)
+  R0 <- max(0, R0n + 0.1*gamma*I0n)
+  out1[i,5] <- S0
+  out1[i,6] <- E0
+  out1[i,7] <- I0
+  out1[i,8] <- R0
+  
+}
+
+plot (1:100, out1[,1], type ="l", ylim =c(0,82000000))
+lines (1:100, out1[,2], col ="orange" )
+lines (1:100, out1[,3], col ="red" )
+lines (1:100, out1[,4], col ="seagreen")
+plot (1:100, out1[,5], type ="l", ylim =c(0,82000000))
+lines (1:100, out1[,6], col ="yellow" )
+lines (1:100, out1[,7], col ="brown")
+lines (1:100, out1[,8], col ="pink")
 
 
   
